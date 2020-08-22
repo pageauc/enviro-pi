3ba4537b208bcfde8bde2598dd123c1074b61659
Claude Pageau <pageauc@gmail.com>
2020-08-22 10:45:12 -0400

    Added DEBUG_MODE

--- writer.py
+++ writer.py
@@ -7,7 +7,10 @@ import time
 import datetime
 from sense_hat import SenseHat
 
-SLEEP = 300  # seconds between data saves to database.
+# User Variables
+#---------------
+DEBUG_MODE = False  # Test sensehat.  Get readings every 5 seconds.  No Data will be saved to sensehat.db
+SLEEP_SEC = 300    # seconds between data saves to sqlite database sensehat.db
 
 sense = SenseHat()
 sense.clear()
@@ -31,6 +34,15 @@ if __name__ == "__main__":
     temperature_from_pressure = round(sense.get_temperature_from_pressure(), 1)
 
     init_db()
+    
+    if DEBUG_MODE:
+        print("Note: DEBUG=True")
+        print("Data will NOT be saved to sensehat.db")
+        print("Readings every 5 seconds to Test if Sensehat hardware is working OK.")
+        print("")
+    else:
+        print("Saving Data to sensehat.db every %i seconds" % SLEEP_SEC)
+    
     while True:
         humidity = round(sense.humidity, 1)
         pressure = round(sense.get_pressure(), 2)
@@ -40,12 +52,15 @@ if __name__ == "__main__":
 
         con = lite.connect("sensehat.db")
         with con:
-            print("%s - Inserting data." % datetime.datetime.now().isoformat())
+            print("Timestamp= %s  Percent Hum= %0.2f  Deg C= %0.2f  Bar Press= %0.2f" %
+                  (datetime.datetime.now().isoformat(),
+                   humidity, temperature_from_humidity, pressure))
+            if DEBUG_MODE:
+                time.sleep(5)
+                continue
+
             cur = con.cursor()
             command = "INSERT INTO sensehat VALUES(%i,%0.2f,%0.2f,%0.2f,%0.2f)" % (int(
                 time.time()), humidity, pressure, temperature_from_humidity, temperature_from_pressure)
             cur.execute(command)
-
-        print("%s - Sleeping for %i seconds." %
-              (datetime.datetime.now().isoformat(), SLEEP))
-        time.sleep(SLEEP)
+        time.sleep(SLEEP_SEC)
